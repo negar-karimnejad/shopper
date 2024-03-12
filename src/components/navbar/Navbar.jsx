f2ef19b5f2020f18e5b30eea83dc84bfbcd96c2e
negar <negar.karimnejad@gmail.com>
2024-03-12 13:21:19 +0330

    set up dark mode 🌜

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,41 +1,19 @@
-import { useEffect, useState } from "react";
+import { HiMoon, HiSun } from "react-icons/hi2";
+import useDarkMode from "../../hooks/useDarkMode";
 import Logo from "./Logo";
 import NavLinks from "./NavLinks";
 import UserLinks from "./UserLinks";
 // absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none
 
 function Navbar() {
-  const [theme, setTheme] = useState(
-    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
-  );
-  const handleToggle = (e) => {
-    if (e.target.checked) {
-      setTheme("dark");
-    } else {
-      setTheme("light");
-    }
-  };
-
-  useEffect(() => {
-    localStorage.setItem("theme", theme);
-    const localTheme = localStorage.getItem("theme");
-    document.querySelector("html").setAttribute("data-theme", localTheme);
-  }, [theme]);
-  console.log(theme);
-
+  const { handleToggle, theme } = useDarkMode();
   return (
     <nav className="flex justify-between items-center bg-slate-50 text-slate-800 dark:text-stone-200 dark:bg-slate-800">
       <Logo />
       <NavLinks />
       <UserLinks />
-      <div>
-        <label htmlFor="">
-          <input
-            type="checkbox"
-            onChange={handleToggle}
-            checked={theme === "light" ? false : true}
-          />
-        </label>
+      <div className="cursor-pointer" onClick={handleToggle}>
+        {theme === "light" ? <HiSun size={24} /> : <HiMoon size={24} />}
       </div>
     </nav>
   );
---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+function useDarkMode() {
+  const [theme, setTheme] = useState(localStorage.theme || "light");
+  const colorTheme = theme === "dark" ? "light" : "dark";
+
+  const handleToggle = () => {
+    setTheme(colorTheme);
+  };
+  useEffect(() => {
+    const root = window.document.documentElement;
+    root.classList.remove(colorTheme);
+    root.classList.add(theme);
+    localStorage.setItem("theme", theme);
+  }, [theme, colorTheme]);
+
+  return { handleToggle, theme };
+}
+
+export default useDarkMode;
