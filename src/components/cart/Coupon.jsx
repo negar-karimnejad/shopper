49ef31ae5fc49b2911ec3dbd6b7f854598006fe2
negar <negar.karimnejad@gmail.com>
2024-03-27 14:10:06 +0330

    protected route

--- src/components/cart/Coupon.jsx
+++ src/components/cart/Coupon.jsx
@@ -1,14 +1,31 @@
+import { useState } from 'react';
 import Button from '../Button';
 import Input from '../Input';
+import toast from 'react-hot-toast';
 
 function Coupon() {
+  const [promo, setPromo] = useState('');
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    toast.error('Promo code is invalid');
+    setPromo('');
+  };
+
   return (
     <div className="flex-1">
       <p className="text-slate-500">If you have a promo code, Enter it here.</p>
-      <form className="mt-4 flex">
-        <Input type="text" placeholder="promo code" />
+      <form className="mt-4 flex" onSubmit={submitHandler}>
+        <Input
+          type="text"
+          placeholder="promo code"
+          value={promo}
+          onChange={(e) => setPromo(e.target.value)}
+        />
         <div>
-          <Button variant="blackLessRound">Submit</Button>
+          <Button variant="blackLessRound" type="submit">
+            Submit
+          </Button>
         </div>
       </form>
     </div>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,15 +1,27 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Spinner from '../components/Spinner';
 import CartItems from '../components/cart/CartItems';
 import CartTotals from '../components/cart/CartTotals';
 import Coupon from '../components/cart/Coupon';
+import { useAuth } from '../context/AuthContext';
 import { useCart } from '../context/CartContext';
 
 function Cart() {
+  const navigate = useNavigate();
   const { state } = useCart();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    if (!user?.session) return navigate('/login');
+  }, [navigate, user?.session]);
+
+  if (!user?.session) return <Spinner title="Redirecting..." />;
 
   if (state?.length === 0)
     return (
       <div className="container flex h-[12rem] items-center justify-center pt-10 text-xl">
-        <p className='border-b border-slate-500 p-3'>Your cart is empty 🙁</p>
+        <p className="border-b border-slate-500 p-3">Your cart is empty 🙁</p>
       </div>
     );
 
